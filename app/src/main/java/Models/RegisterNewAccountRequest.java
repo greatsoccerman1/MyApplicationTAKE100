b64f36373d0969e5cd9dbea0f18ffa49607935b4
Curtis <J.Ripley0805@gmail.com>
2021-09-03 15:57:41 -0500

    first commit

---
+++ app/src/main/java/Models/RegisterNewAccountRequest.java
@@ -0,0 +1,23 @@
+package Models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@Builder
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterNewAccountRequest {
+    private String groupName;
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String reqStatus;
+
+}
